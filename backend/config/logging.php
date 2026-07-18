c8544c8336352cda8b466e5268fac4f0b95c4f5a
Moise Otuka <moiseotuka@hmckokolo.cd>
2026-07-18 05:42:38 +0100

    fix: add skeleton files, config, and update Dockerfile

---
+++ backend/config/logging.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'default' => env('LOG_CHANNEL', 'stack'),
+    'channels' => [
+        'stack' => [
+            'driver' => 'stack',
+            'channels' => ['single'],
+            'ignore_exceptions' => false,
+        ],
+        'single' => [
+            'driver' => 'single',
+            'path' => storage_path('logs/laravel.log'),
+            'level' => env('LOG_LEVEL', 'debug'),
+        ],
+        'syslog' => [
+            'driver' => 'syslog',
+            'level' => env('LOG_LEVEL', 'debug'),
+            'facility' => LOG_USER,
+        ],
+        'errorlog' => [
+            'driver' => 'errorlog',
+            'level' => env('LOG_LEVEL', 'debug'),
+        ],
+    ],
+];
